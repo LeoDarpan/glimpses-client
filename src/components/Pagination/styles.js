581d7781455306dfbc408aede81c3dfedbd49a7e
DARPAN ANEJA <leo29791@gmail.com>
2022-03-24 16:24:37 +0530

    Updated spacing for page numbers

--- src/components/Pagination/styles.js
+++ src/components/Pagination/styles.js
@@ -2,7 +2,7 @@ import { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles(() => ({
     ul: {
-        justifyContent: 'space-between',
+        justifyContent: 'between',
         marginTop: '60px',
         padding:'10px 0px',
     }
